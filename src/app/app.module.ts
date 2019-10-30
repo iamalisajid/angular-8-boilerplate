3170ebd9330fc33e5efbaf731bd2bc1d1d0f0fa4
Ali Sajid <ali.sajid@arbisoft.com>
2019-10-31 02:07:44 +0500

    :sparkles:initial-commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {SpinnerComponent} from './shared/spinner.component';
+import { AuthComponent } from './auth/auth.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SpinnerComponent,
+    AuthComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
